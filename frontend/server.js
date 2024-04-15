64119e4b24d26ea7a139960becb256e6c5f76b80
Kolja Kleinschmidt <kolja.kleinschmidt@volkswagen.de>
2024-04-15 20:21:27 +0200

    initial frontend and backend setup

---
+++ frontend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 4242;
+
+app.use(express.static('app'));
+
+app.listen(port, () => {
+    console.log(`Server now listens at http://localhost:${port}`);
+})
