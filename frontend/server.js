c7a6b383ddc3f0f65644a19cbbbaa2a95ff52c7c
Kolja Kleinschmidt <kolja.kleinschmidt@volkswagen.de>
2024-08-10 20:17:11 +0200

    remove old comments

--- frontend/server.js
+++ frontend/server.js
@@ -2,17 +2,6 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const mimeTypes = {
-    '.html': 'text/html',
-    '.css': 'text/css',
-    '.js': 'application/javascript',
-    '.json': 'application/json',
-    '.png': 'image/png',
-    '.jpg': 'image/jpeg',
-    '.ico': 'image/x-icon'
-};
-
-
 const server = http.createServer((req, res) => {
     // Construct the file path
     const filePath = path.join(__dirname, 'app', req.url === '/' ? 'index.html' : req.url);
@@ -79,40 +68,6 @@ function processSSI(data) {
         }
     });
 }
-
-// const server = http.createServer((req, res) => {
-//     // Default to index.html if the root URL is requested
-//     let filePath = path.join(__dirname, 'app', req.url === '/' ? 'index.html' : req.url);
-//     const extname = path.extname(filePath).toLowerCase();
-//     const contentType = mimeTypes[extname] || 'application/octet-stream';
-//
-//     // Prevent directory traversal attacks
-//     if (filePath.indexOf(path.join(__dirname, 'app')) !== 0) {
-//         res.writeHead(403, { 'Content-Type': 'text/html' });
-//         res.end('<h1>403 - Forbidden</h1>');
-//         return;
-//     }
-//
-//     fs.readFile(filePath, (err, content) => {
-//         if (err) {
-//             if (err.code === 'ENOENT') {
-//                 // If the file is not found, serve a 404 page
-//                 fs.readFile(path.join(__dirname, 'app', '404.html'), (err, content) => {
-//                     res.writeHead(404, { 'Content-Type': 'text/html' });
-//                     res.end(content, 'utf8');
-//                 });
-//             } else {
-//                 // For other server errors, send a 500 status code
-//                 res.writeHead(500);
-//                 res.end(`Server Error: ${err.code}`);
-//             }
-//         } else {
-//             // Serve the file with the correct content type
-//             res.writeHead(200, { 'Content-Type': contentType });
-//             res.end(content, 'utf8');
-//         }
-//     });
-// });
 
 const PORT = process.env.PORT || 4242;
 server.listen(PORT, () => {
