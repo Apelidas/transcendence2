197210c80eddf2706ebb3949ca0ef892c0cdc95f
Kolja Kleinschmidt <kolja.kleinschmidt@volkswagen.de>
2024-08-22 18:31:26 +0200

    refactor: transform signupview method into SignUpView class to exempt it from securitycheck

--- backend/transcendence/adapter/signup.py
+++ backend/transcendence/adapter/signup.py
@@ -2,26 +2,27 @@ import json
 
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.views import APIView
 
 from transcendence.models import CustomUser
 
 
-@csrf_exempt
-def signup_view(request):
-    if request.method != "POST":
-        return JsonResponse({'error': request.method}, status=409)
-    data = json.loads(request.body)
-    email = data['email']
-    password = data['password']
-    if not email or not password:
-        return JsonResponse({'error': 'Missing credentials'}, status=400)
-    if CustomUser.objects.user_exists(email) is True:
-        return JsonResponse({'error': 'User exists already'}, status=409)
-    user = create_custom_user(email, password)
-    if user is None:
-        return JsonResponse({'error': 'something'}, status=409)
-    user.save()
-    return JsonResponse({'message': 'Hello World'}, status=200)
+class SignUpView(APIView):
+    authentication_classes = []
+
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data.get('email')
+        password = data.get('password')
+        if not email or not password:
+            return JsonResponse({'error': 'Missing credentials'}, status=400)
+        if CustomUser.objects.user_exists(email) is True:
+            return JsonResponse({'error': 'User exists already'}, status=409)
+        user = create_custom_user(email, password)
+        if user is None:
+            return JsonResponse({'error': 'something went wrong'}, status=409)
+        user.save()
+        return JsonResponse({'message': 'Sign Up successful'}, status=200)
 
 
 def create_custom_user(email, password):
