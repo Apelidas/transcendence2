a7a2bcc24771daf6f5939ed101f4b755dceeb00e
root <root@Debian.myguest.virtualbox.org>
2024-10-08 15:52:33 +0200

    feat: qrcode creation is now supported

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -76,11 +76,32 @@ document.addEventListener('DOMContentLoaded', function () {
     handleRouting();
 });
 
-async function check_2fa() {
-    const secret = window.otplib.authenticator.generateSecret(); // 'KVKFKRCPNZQUYMLXOVYDSQKJKZDTSRLD';
-    console.log("2FA secret = " + secret);
-    const token = window.otplib.authenticator.generate(secret);
+const secret = window.otplib.authenticator.generateSecret();
+
+function auth_2fa_get_token(secret) {
+    let token = window.otplib.authenticator.generate(secret);
     console.log("2FA token = " + token);
+    return token;
+}
+
+async function check_2fa() {
+    console.log("2FA secret = " + secret);
+
+    // qrcode
+    const otpauth = window.otplib.authenticator.keyuri("daniel", "transcendence", secret);
+    console.log("2FA otpauth = " + otpauth);
+
+    let qrcode = new QRCode(document.getElementById("qrcode"), {
+        text: otpauth,
+        width: 128,
+        height: 128,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.H
+    });
+
+    const token = auth_2fa_get_token(secret);
+    setInterval(auth_2fa_get_token, 10000, secret);
     try {
         const isValid = window.otplib.authenticator.check(token, secret);
         console.log("2FA valid check!");
