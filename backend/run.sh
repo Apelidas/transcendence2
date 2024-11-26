c82ae6709d1cb8b895c20593f8de45c10681a4db
Daniel Brandtner <daniel@techmuesli.de>
2024-11-26 18:50:50 +0100

    mkcert running, but not working

--- backend/run.sh
+++ backend/run.sh
@@ -2,4 +2,5 @@
 
 python manage.py migrate
 
-python manage.py runserver_plus 0.0.0.0:8000 --cert-file cert.pem --key-file key.pem
+#python manage.py runserver_plus 0.0.0.0:8000 --cert-file cert.pem --key-file key.pem
+python manage.py runsslserver --certificate cert.pem --key key.pem
