58fcf06936a7c9c30e5f778ed66d442355fcc1f6
Daniel Brandtner <daniel@techmuesli.de>
2024-11-26 20:01:15 +0100

    switch to runserver_plus

--- backend/run.sh
+++ backend/run.sh
@@ -3,4 +3,4 @@
 python manage.py migrate
 
 #python manage.py runserver_plus 0.0.0.0:8000 --cert-file cert.pem --key-file key.pem
-python manage.py runsslserver --certificate cert.pem --key key.pem
+python manage.py runserver_plus --cert-file cert.pem --key-file key.pem
--- backend/server/settings.py
+++ backend/server/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sslserver',
+    'django_extensions',
     'corsheaders',
     'transcendence.apps.TranscendenceConfig',
     'rest_framework',
