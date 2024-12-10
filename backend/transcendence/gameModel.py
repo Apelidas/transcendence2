75ebdc68e3ed6ff68fcca8b2185eb55473e7bdfa
Kolja Kleinschmidt <kolja.kleinschmidt@volkswagen.de>
2024-12-10 18:27:26 +0100

    create GameData table

--- backend/transcendence/gameModel.py
+++ backend/transcendence/gameModel.py
@@ -1,35 +1,42 @@
 from django.db import models
 from django.conf import settings
-from django.utils import timezone
 
-class Game(models.Model):
-    # Unique game ID
+
+class GameDataManager(models.Manager):
+    def getAllGamesWonBy(self, user, againstAi):
+        return self.filter(user=user, user_won=True, against_ai=againstAi)
+
+    def getAllGamesPlayedBy(self, user, againstAi):
+        return self.filter(user=user, against_ai=againstAi)
+
+    def getMostWinsInARow(self, user, againstAi):
+        games = self.filter(user=user, against_ai=againstAi).order_by('played_at')
+        max_streak = 0
+        current_streak = 0
+
+        for game in games:
+            if game.user_won:
+                current_streak += 1
+                max_streak = max(max_streak, current_streak)
+            else:
+                current_streak = 0 
+
+        return max_streak
+
+
+class GameData(models.Model):
     id = models.AutoField(primary_key=True)
-
-    # Game type, with options 'pong' or 'tic tac toe'
-    PONG = 'pong'
-    TIC_TAC_TOE = 'tic_tac_toe'
-    GAME_TYPE_CHOICES = [
-        (PONG, 'Pong'),
-        (TIC_TAC_TOE, 'Tic Tac Toe')
-    ]
-    gametype = models.CharField(max_length=15, choices=GAME_TYPE_CHOICES)
-
-    # End score, stored as a string like '9:5'
-    endscore = models.CharField(max_length=5)
-
+    left_score = models.IntegerField()
+    right_score = models.IntegerField()
+    against_ai = models.BooleanField()
+    played_at = models.DateTimeField()
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
     user_won = models.BooleanField()
 
-    against_ai = models.BooleanField()
-
-    # Date and time the game was played, default to current datetime
-    played_at = models.DateTimeField(default=timezone.now)
-
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    objects = GameDataManager()
 
     def __str__(self):
-        return f'Game {self.id} - {self.gametype}'
-
-    class Meta:
-        # Optionally you can add more metadata options, like ordering by `played_at`
-        ordering = ['-played_at']
+        return f"Game {self.id}: {self.user} {'won' if self.user_won else 'lost'} on {self.played_at}"
