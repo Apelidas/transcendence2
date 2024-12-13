827be126c35f85df3d352b9459b9399fb0b585b9
Daniel Brandtner <dbrandtn@c2r10s8.42wolfsburg.de>
2024-12-13 16:35:36 +0100

    prep ttt bracket

--- frontend/app/games/ticTacToe/ticTacToe.js
+++ frontend/app/games/ticTacToe/ticTacToe.js
@@ -14,8 +14,10 @@ document.getElementById('ticTacToeAiButton').addEventListener('click', function(
 
 document.getElementById('ticTacToeTournButton').addEventListener('click', function() {
     
+    document.getElementById("startTTTTournament").style.display = "inline";
     document.getElementById("tttTournPlayer1Name").disabled = false;
 	document.getElementById("tttTournPlayer2Name").disabled = false;
     document.getElementById("tttTournPlayer3Name").disabled = false;
 	document.getElementById("tttTournPlayer4Name").disabled = false;
+    document.getElementById("ttt-tournament-bracket").style.display = "none";
 });
--- frontend/app/games/ticTacToe/ticTacToeTourn/ticTacToeTourn.js
+++ frontend/app/games/ticTacToe/ticTacToeTourn/ticTacToeTourn.js
@@ -1,10 +1,11 @@
 
 document.getElementById("startTTTTournament").addEventListener('click', () => {
 
+    document.getElementById("startTTTTournament").style.display = "none";
     document.getElementById("tttTournPlayer1Name").disabled = true;
 	document.getElementById("tttTournPlayer2Name").disabled = true;
     document.getElementById("tttTournPlayer3Name").disabled = true;
 	document.getElementById("tttTournPlayer4Name").disabled = true;
-    const bracket = document.getElementById("TTTGameBracket");
+    const bracket = document.getElementById("ttt-tournament-bracket");
     bracket.style.display = 'grid';
 });
