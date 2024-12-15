7590ce0bf084738609b9d84ee975b8cb2e8fe6e1
Kolja Kleinschmidt <kkleinsc@c2r9s6.42wolfsburg.de>
2024-12-15 23:27:13 +0100

    fixes errors when response undefined

--- frontend/app/1-services/2fa.js
+++ frontend/app/1-services/2fa.js
@@ -2,11 +2,13 @@
 const mfaDataEndpoint = BACKEND_PATH + '/mfa_data/'
 
 async function auth_2fa_get_secret() {
-
-    const response = await fetchWithToken(mfaDataEndpoint, 'POST');
-    const data = await response.json();
-
-    if (!data.username || !data.secret)
-        throw("Missing user data");
-    return {secret: data.secret, username: data.username}
+    try{
+        const response = await fetchWithToken(mfaDataEndpoint, 'POST');
+        const data = await response.json();
+    
+        if (!data.username || !data.secret)
+            throw("Missing user data");
+        return {secret: data.secret, username: data.username}
+    }
+    catch(exception){}
 }
--- frontend/app/1-services/tournament.js
+++ frontend/app/1-services/tournament.js
@@ -2,20 +2,23 @@
 
 
 async function createTournament(isPong, participants){
-    const response = await fetchWithToken(tournamentEndpoint, 'POST', {}, {
-        'isPong': isPong,
-        'participants': participants
-    })
-    if (response.status === 200){
-        json = await response.json();
-        if (verifyId(json)){
-            setCookie('tournamentId', json.id, 1000000);
-            return json.id;
+    try{
+        const response = await fetchWithToken(tournamentEndpoint, 'POST', {}, {
+            'isPong': isPong,
+            'participants': participants
+        })
+        if (response.status === 200){
+            json = await response.json();
+            if (verifyId(json)){
+                setCookie('tournamentId', json.id, 1000000);
+                return json.id;
+            }
         }
+        if (response.status === 403)
+            return true;
+        alert('tournament could not be saved to backend' + response.error);
+    }catch(exception){
     }
-    if (response.status === 403)
-        return true;
-    alert('tournament could not be saved to backend' + response.error);
     return false;
 }
 
