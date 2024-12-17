3b540cd9fca1b5890ae9bec7349f2cd1400610f4
Kolja Kleinschmidt <kolja.kleinschmidt@volkswagen.de>
2024-12-17 21:14:30 +0100

    adding a wait fro database to bakcend

--- backend/run.sh
+++ backend/run.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+while true; do
+  echo "Trying to connect to ${DB_NAME} \n"
+  pg_isready -U $DB_USER -h $DB_HOST -d $DB_NAME -p $DB_PORT
 
+  if [ $? -eq 0 ]; then
+    break;
+  else
+    sleep 10
+  fi
+done
 python manage.py makemigrations
 python manage.py migrate
 
